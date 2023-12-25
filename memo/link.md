84d12f38d05f7c4158db236ba7dcbb23a3039a34
koya-kimura <2ktmnri8@gmail.com>
2023-12-25 20:20:56 +0900

    2023/12/25-20:20:56 save

---
+++ memo/link.md
@@ -0,0 +1,3 @@
+## タメになってるリンクまとめ
+
+- [GLSLを使ってVJしてきたのでそこで使ったこととかまとめ](https://qiita.com/edo_m18/items/51bbda8ebd81742e19c8)
--- src/2023-12-25-19-09-07.glsl
+++ src/2023-12-25-19-09-07.glsl
@@ -1,8 +1,23 @@
+// 六角形を描く
+// 参考文献 : https://qiita.com/edo_m18/items/37d8773a5295bc6aba3d
+// 補助参考文献 : 内積の補足 https://youtu.be/j_zr0D656hM?si=UhqwQGq5hrW6gm7d
+
 void mainImage(out vec4 fragColor,in vec2 fragCoord)
 {
-    vec2 uv=fragCoord/iResolution.xy;
+    vec2 uv=(fragCoord-.5*iResolution.xy)/min(iResolution.x, iResolution.y);
+    uv *= 4.;
 
-    vec3 col=.5+.5*cos(iTime+uv.xyx+vec3(0,2,4));
+    vec3 col=vec3(0);
+
+    vec2 r = normalize(vec2(1.,1.73));
+    vec2 h = r * 0.5;
+    vec2 a = mod(uv, r) - h;
+    vec2 b = mod(uv-h, r) - h;
+
+    vec2 gv = length(a) < length(b) ? a : b;
+    vec2 id = uv-gv;
+
+    col.b = length(id) *0.2 + pow(sin(length(id) + iTime), 4.0)*0.1;
 
     fragColor=vec4(col,1.);
 }
