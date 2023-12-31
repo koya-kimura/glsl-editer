d6414571af99dd35761b25aec5acaa676b6a9492
koya-kimura <2ktmnri8@gmail.com>
2023-12-31 21:12:49 +0900

    2023/12/31-21:12:49 save

--- src/2023-12-31-20-58-34.glsl
+++ src/2023-12-31-20-58-34.glsl
@@ -1,6 +1,7 @@
-#iChannel1"https://66.media.tumblr.com/tumblr_mcmeonhR1e1ridypxo1_500.jpg"
-
 #define PI 3.1415926535
+
+#iChannel1 "https://2.bp.blogspot.com/-sCYd5MsquF4/VCOJS6eoLvI/AAAAAAAAmvE/EDFPg_-YdmQ/s800/eto_mark04_usagi.png"
+#iChannel2 "https://1.bp.blogspot.com/-7OzCCLysaWY/VCOJTY7fF5I/AAAAAAAAmvY/HqoY2F8aEqA/s800/eto_mark05_tatsu.png"
 
 float random(vec2 st){
     return fract(sin(dot(st.xy,vec2(12.9898,78.233)))*43758.5453123);
@@ -16,8 +17,7 @@ float atan2(float y,float x){
 
 vec2 xy2pol(vec2 xy){
     return vec2(atan2(xy.y,xy.x),length(xy));
-}   
-
+}
 
 vec2 pol2xy(vec2 pol){
     return pol.y*vec2(cos(pol.x),sin(pol.x));
@@ -26,15 +26,16 @@ vec2 pol2xy(vec2 pol){
 void mainImage(out vec4 fragColor,in vec2 fragCoord)
 {
     vec2 uv=fragCoord/iResolution.xy;
-    uv = (uv-0.5)*2.;
+    uv = floor(uv*100.)/100.;
 
-    float l = length(uv);
+    vec4 col1 = texture2D(iChannel1, uv);
 
-    uv *= rot(PI/l + iTime);
+    vec4 col2 = texture2D(iChannel2,uv);
 
-    // uv = fract(uv*10.);
+    vec4 col = vec4(1.);
 
-    vec4 col = texture2D(iChannel1, uv);
+    float a = 1.-mod(iTime/10.,1.);
+    col.rgb = uv.y < a ? col1.rgb : col2.rgb;
 
     fragColor=col;
 }
