190b6c14c68939a58a27831136101911b74f69dc
koya-kimura <2ktmnri8@gmail.com>
2024-01-04 11:24:21 +0900

    2024/01/04-11:24:21 save

---
+++ src/2024-01-04-11-14-34.glsl
@@ -0,0 +1,50 @@
+#define PI 3.1415926535
+
+float random(vec2 st){
+    return fract(sin(dot(st.xy,vec2(12.9898,78.233)))*43758.5453123);
+}
+
+mat2 rot(float angle){
+    return mat2(cos(angle),-sin(angle),sin(angle),cos(angle));
+}
+
+float atan2(float y,float x){
+    return x==0.?sign(y)*PI/2.:atan(y,x);
+}
+
+vec2 xy2pol(vec2 xy){
+    return vec2(atan2(xy.y,xy.x),length(xy));
+}
+
+vec2 pol2xy(vec2 pol){
+    return pol.y*vec2(cos(pol.x),sin(pol.x));
+}
+
+float easeInSine(float x){
+    return 1.-cos((x*3.14159265359)/2.);
+}
+
+float easeOutSine(float x){
+    return sin((x*3.14159265359)/2.);
+}
+
+float easeInOutSine(float x){
+    return-(cos(3.14159265359*x)-1.)/2.;
+}
+
+// -------------------------
+
+#iChannel0"../assets/image/night-meat.png"
+
+void mainImage(out vec4 fragColor,in vec2 fragCoord)
+{
+    vec2 uv=fragCoord/iResolution.xy;
+
+    uv=xy2pol((uv-.5)*2.);
+
+    uv.y-=iTime;
+
+    vec4 col = vec4(vec3((step(fract(uv.y*cos(uv.x)), .1)*random(uv))+.1), 1.);
+
+    fragColor=col;
+}
