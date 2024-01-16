0f1bf2d7a4e25532668ee8f0e2ead7e70ed662ee
koya-kimura <2ktmnri8@gmail.com>
2024-01-17 00:42:11 +0900

    2024/01/17-00:42:11 save

--- src/2024-01-17-00-40-01.glsl
+++ src/2024-01-17-00-40-01.glsl
@@ -34,21 +34,37 @@ float easeInOutSine(float x){
 
 // -------------------------
 
+#iChannel0"../assets/image/night-shibuya.png"
+
+vec3 colors[5];
+
 void mainImage(out vec4 fragColor,in vec2 fragCoord)
 {
+    colors[0]=vec3(1.,.71,.72);// #FFB4B8
+    colors[1]=vec3(.94,.29,.16);// #EF4B28
+    colors[2]=vec3(.04,.34,.23);// #0A563A
+    colors[3]=vec3(1.,.74,.33);// #FFBC54
+    colors[4]=vec3(.93,.91,.88);// #ECE9E0
+
     vec2 uv=fragCoord/iResolution.xy;
-    uv = fract(uv*5.);
+    uv = fract(uv*10.);
 
     uv = xy2pol((uv-.5)*2.);
 
     uv.x = min(mod(uv.x, PI/4.), PI/4.-mod(uv.x,PI/4.));
     uv.x += uv.y;
 
-    float gray = step(uv.x, 1.1);
+    float gray = 1.;
 
-    gray -= step(uv.y,.6);
+    gray -= step(uv.x, 1.1);
 
-    gray += step(uv.x,.7);
+    gray += step(uv.y,.6)*.1;
 
-    fragColor=vec4(vec3(gray), 1.);
+    gray += step(uv.x,.7)*.1;
+
+    gray += step(uv.x,.5)*.1;
+
+    vec3 col = colors[int(floor(gray*5.))];
+
+    fragColor=vec4(col, 1.);
 }
